4042154d78c97ea72d4e05901a33ae1d0f1398f6
shrimpie <1210517@tongji.edu.cn>
2019-01-05 23:51:35 -0500

    update

---
+++ build/precache-manifest.468ea996c723a3a82fcbd5d90eeed2e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "70ebf5f2232d46782b6b",
+    "url": "/static/css/main.fff82c45.chunk.css"
+  },
+  {
+    "revision": "70ebf5f2232d46782b6b",
+    "url": "/static/js/main.70ebf5f2.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c4b8bda0f28ed320c29419df5e0e134",
+    "url": "/index.html"
+  }
+];
